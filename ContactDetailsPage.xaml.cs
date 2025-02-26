d29746546f42675666a35f48f9373821cffc1944
huttonsy <huttonsy@mail.uc.edu>
2025-02-25 20:35:30 -0500

    working as expected

--- ContactDetailsPage.xaml.cs
+++ ContactDetailsPage.xaml.cs
@@ -12,5 +12,14 @@ namespace MauiContactApp.Views
         {
             await Navigation.PopAsync();
         }
+        private async void OnContactSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is MauiContactApp.Contact selectedContact)
+            {
+                await Navigation.PushAsync(new ContactDetailsPage(selectedContact));
+                ((CollectionView)sender).SelectedItem = null; 
+            }
+        }
+
     }
 }
